ac953bd9c8824af478a63c57749822e84c36f2c6
Luca SJ <saintjore.luca@gmail.com>
2024-06-30 04:45:43 +0200

    Supression d'un import qui déclenchait une erreur

--- back/src/main/java/com/openclassrooms/starterjwt/security/jwt/JwtUtils.java
+++ back/src/main/java/com/openclassrooms/starterjwt/security/jwt/JwtUtils.java
@@ -2,7 +2,6 @@ package com.openclassrooms.starterjwt.security.jwt;
 
 import java.util.Date;
 
-import jdk.nashorn.internal.objects.annotations.Constructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
